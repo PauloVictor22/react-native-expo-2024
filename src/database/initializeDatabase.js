8334f00b544e5d9929caae7850954b2b61093c8f
PauloMuhammad <160732116+PauloVictor22@users.noreply.github.com>
2024-09-18 13:33:33 +0000

    [fix] Create Payments Database

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,6 +1,8 @@
 export async function initializeDatabase(database) {
     try {
         await database.execAsync(`
+            DROP TABLE IF EXISTS payments;
+
             DROP TABLE IF EXISTS users;
 
             CREATE TABLE IF NOT EXISTS users (
@@ -11,6 +13,20 @@ export async function initializeDatabase(database) {
                 role TEXT NOT NULL DEFAULT 'USER',
                 created_at DATE DEFAULT CURRENT_TIMESTAMP,
                 updated_at DATE
+            );
+
+            CREATE TABLE IF NOT EXISTS payments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            user_cadastro INTEGER NOT NULL,
+            valor_pago REAL NOT NULL,
+            data_pagamento DATE NOT NULL,
+            observacao TEXT,
+            created_at DATE DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATE,
+            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_cadastro) REFERENCES users(id),
+
             );
             
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
